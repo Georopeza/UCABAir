353e2f1da99c7c1fa76c434d77e931320c59a277
Georopeza <149003391+Georopeza@users.noreply.github.com>
2024-06-23 03:53:32 +0000

    Se inicio el modulo de orden a aliados y de reponer inventario

---
+++ my-app/src/routes/admin/HomeAdmin/registrar/proyecto/configuracionProyecto/configurarEtapa/configurarActividad/asignarEmpleado/+page.svelte
@@ -0,0 +1,36 @@
+<script lang=ts>
+    import ConfigurarActividad from '$lib/components/configurarActividad.svelte'
+    import AgregarEmpleados from '$lib/components/agregarEmpleados.svelte';
+    import AgregarMaquinaria from '$lib/components/agregarMaquinaria.svelte';
+	import ConfigurarProyecto from '$lib/components/configurarProyecto.svelte';
+</script>
+
+<body>
+    <div class="ConfigurarActividad">
+        <h2>Agregar empleado</h2>
+                <div class="AgregarEmpleados">
+                    <AgregarEmpleados/>
+                </div>
+                <a href="/admin/HomeAdmin/registrar/proyecto/configuracionProyecto/configurarEtapa/configurarActividad/asignarEmpleado">
+                    <button>Agregar Otro Empleado</button>
+                </a> 
+                <a href="/admin/HomeAdmin/registrar/proyecto/configuracionProyecto/configurarEtapa/configurarActividad/asignarMaquinaria">
+                    <button>Agregar Maquinaria</button>
+                </a>      
+                <a href="/admin/HomeAdmin/proyectos">
+                    <button>Terminado</button>
+                </a>   
+    </div>  
+</body>
+
+<style>
+    button {
+        background-color: #050505;
+        color: #fff;
+        border: none;
+        border-radius: 3px;
+        padding: 10px 20px;
+        cursor: pointer;
+        font-size: 16px;
+    }
+</style>
