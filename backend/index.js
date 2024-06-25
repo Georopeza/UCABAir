7986307f0b75233d4594807eaa60b0dc9b954304
SofiaW <117039398+SofiShiny@users.noreply.github.com>
2024-06-25 19:59:15 -0400

    prueba bd

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,10 @@ const morgan= require('morgan');
 const cors= require('cors');
 const taskRoutes = require('./routes/tasks.routes');
 const app= express();
-const {direccion}=require('./config')
-
-const FRONTEND_URL=direccion.frontend
-app.use(
-    cors({
-      origin: FRONTEND_URL
-    })
-  );
   
+app.use(cors());
 app.use(morgan('dev'))
-app.use(express.json());
+app.use(express.json()); 
 app.use(taskRoutes)
 app.use((err,req,res,next) => {
     return res.json({
